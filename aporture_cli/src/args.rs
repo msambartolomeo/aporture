2bfee4f0ef59cef48443b01d5a6025b90cc4adb0
Mauro Sambartolomeo <msambartolomeo@itba.edu.ar>
2023-11-26 16:23:53 -0300

    add basic cli argument parsing

---
+++ aporture_cli/src/args.rs
@@ -0,0 +1,70 @@
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(name = "Aporture", author, version, about)]
+pub struct AportureArgs {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Commands {
+    /// Send a file
+    Send {
+        file_name: String,
+
+        #[command(flatten)]
+        method: Option<SendMethod>,
+
+        #[arg(short, long, value_names(["NAME"]))]
+        save: Option<String>,
+    },
+    /// Recieve a file
+    Recieve {
+        #[command(flatten)]
+        method: RecieveMethod,
+
+        #[arg(short, long, value_names(["NAME"]))]
+        save: Option<String>,
+    },
+    /// List registered contacts
+    Contacts,
+    /// Pair a new contact
+    Pair {
+        #[command(subcommand)]
+        command: PairCommand,
+    },
+}
+
+#[derive(Debug, Args)]
+#[group(multiple = false)]
+pub struct SendMethod {
+    #[arg(short, long)]
+    pub passphrase: Option<String>,
+
+    #[arg(short, long, value_names(["NAME"]))]
+    pub contact: Option<String>,
+}
+
+#[derive(Debug, Args)]
+#[group(required = true, multiple = false)]
+pub struct RecieveMethod {
+    pub passphrase: Option<String>,
+
+    #[arg(short, long, value_names(["NAME"]))]
+    pub contact: Option<String>,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum PairCommand {
+    Start {
+        #[arg(short, long)]
+        save: String,
+    },
+    Complete {
+        passphrase: String,
+
+        #[arg(short, long)]
+        save: String,
+    },
+}
--- aporture_cli/src/main.rs
+++ aporture_cli/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+mod args;
+use args::{AportureArgs, Commands};
+
+use anyhow::Result;
+use clap::Parser;
+
+fn main() -> Result<()> {
+    let args = AportureArgs::parse();
+
+    dbg!(&args);
+
+    match args.command {
+        Commands::Send {
+            file_name,
+            method,
+            save,
+        } => todo!(),
+        Commands::Recieve { method, save } => todo!(),
+        Commands::Contacts => todo!(),
+        Commands::Pair { command: _ } => todo!(),
+    }
 }
